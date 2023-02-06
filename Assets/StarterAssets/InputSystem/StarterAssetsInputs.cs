187de4f35907b5f641f1a5ca05529c3849339339
Oenothera <76833065+oen0thera@users.noreply.github.com>
2023-02-06 20:16:00 +0900

    mergingt
    tttt

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -18,12 +18,7 @@ namespace StarterAssets
 		public bool shoot;
 		//김기범 investigate - R키 누르면 해당 오브젝트 조사
 		public bool investigate;
-
-		//김원진 interaction - E키 누르면 해당 오브젝트와 상호작용
-		public bool interaction;
-
-		//김원진 inventory - I키 누르면 Inventory UI 활성화
-		public bool inventory;
+	
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -74,94 +69,45 @@ namespace StarterAssets
 			InvestigateInput(value.isPressed);
 		}
 
-		//김원진 - InteractionInput 함수에 버튼이 눌렸는지 안눌렸는지 값 넘겨주는 함수
-		public void OnInteraction(InputValue value)
-		{
-			InteractionInput(value.isPressed);
-		}
-
-		//김원진 - InventoryInput 함수에 버튼이 눌렸는지 안눌렸는지 값 넘겨주는 함수
-		public void OnInventory(InputValue value)
-		{
-			InventoryInput(value.isPressed);
-		}
-
-
 #endif
 
-		//김원진 - 인벤토리 상태시 캐릭터 조작 불가 상태 활성화
+
 		public void MoveInput(Vector2 newMoveDirection)
 		{
-			if (inventory==false)
 			move = newMoveDirection;
 		} 
 
 		public void LookInput(Vector2 newLookDirection)
 		{
-            if (inventory == false)
-            look = newLookDirection;
+			look = newLookDirection;
 		}
 
 		public void JumpInput(bool newJumpState)
 		{
-            if (inventory == false)
 			jump = newJumpState;
 		}
 		
 		public void SprintInput(bool newSprintState)
 		{
-            if (inventory == false)
 			sprint = newSprintState;
 		}
 
 		//김기범 - Aiminput함수추가
 		public void AimInput(bool newAimState)
 		{
-            if (inventory == false)
 			aim = newAimState;
 		}
 		//김기범 -  ShootInput 함수 추가
 		public void ShootInput(bool newShootState)
 		{
-            if (inventory == false)
-                shoot = newShootState;
+			shoot = newShootState;
 		}
 		//김기범 - 조사버튼 함수 추가;
 		public void InvestigateInput(bool newInvestigateState)
 		{
-            if (inventory == false)
-            investigate = newInvestigateState;
-			
+			investigate = newInvestigateState;
 		}
 
-		//김원진 - 상호작용 함수 추가;
-		public void InteractionInput(bool newInteractionState)
-		{
-            if (inventory == false)
-            interaction = newInteractionState;
-            
-        }
-
-		//김원진 - 인벤토리 함수 추가;
-		//김원진 - 인벤토리가 열려있을때 누르면 닫히도록 함
-		public void InventoryInput(bool newInventoryState)
-		{
-			if (inventory == false)
-			{
-				move = new Vector2(0,0);
-                inventory = newInventoryState;
-				Cursor.visible = true;
-				Cursor.lockState = CursorLockMode.None;
-            }
-			else
-			{
-                Cursor.visible = false;
-				Cursor.lockState = CursorLockMode.Locked;
-				inventory = false;
-            }
-			
-			 
-		}
 
 		private void OnApplicationFocus(bool hasFocus)
 		{
